07b9223740a3c2a7f3d7a86f848dada35f366ce8
Pierre Wieser <pwieser@trychlos.org>
2023-04-28 16:23:54 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:toggle-switch',
-    version: '0.3.0',
+    version: '0.3.1-rc',
     summary: 'A toggle switch Blaze component',
     git: 'https://github.com/trychlos/pwix-toggle-switch',
     documentation: 'README.md'
